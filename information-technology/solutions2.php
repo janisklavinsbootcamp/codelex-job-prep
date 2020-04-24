6757126024eb1b3f2844b4928bd681c0fa6fa563
Janis Klavins <janis.klavins24@gmail.com>
2020-04-24 10:49:06 +0300

    second commit

---
+++ information-technology/solutions2.php
@@ -0,0 +1,18 @@
+<?php
+
+
+function maxProfit(array $pricesAndPurchases): int
+{
+    throw new \Exception('Not implemented.');
+}
+
+function stringCost(string $a, string $b,
+                    int $insertionCost, int $deletionCost, int $replacementCost): int
+{
+    throw new \Exception('Not implemented.');
+}
+
+function incrementalMedian(array $values): array
+{
+    throw new \Exception('Not implemented.');
+}
