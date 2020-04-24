c69f66d591140c3fbab9e7d324017fced9b33e72
Janis Klavins <janis.klavins24@gmail.com>
2020-04-25 00:09:06 +0300

    tenth commit

--- information-technology/solutions.php
+++ information-technology/solutions.php
@@ -9,12 +9,13 @@ function findOddEvenPair(array $numbers): int
             array_push($indexes, (int)$i);
         }
     }
-    //var_dump($indexes);
     $randomIndex = array_rand($indexes);
     return $indexes[$randomIndex];
 }
 
-//echo findOddEvenPair([1, 2, 4, 3, 7]);
+echo 'findOddEvenPair result: ' .PHP_EOL;
+$testInput = [1, 2, 4, 3, 7];
+echo findOddEvenPair($testInput) .PHP_EOL;
 
 
 class SummationService
@@ -37,8 +38,9 @@ class SummationService
     }
 }
 
-//$new = new SummationService([-1, 0, 2, 7, -15]);
-//echo $new->sum(2, 4);
+echo 'SummationService result: ' .PHP_EOL;
+$newTest = new SummationService([-1, 0, 2, 7, -15]);
+echo $newTest->sum(2, 4) .PHP_EOL;
 
 
 function longestSubstr(string $string): string
@@ -58,19 +60,21 @@ function longestSubstr(string $string): string
     //reset all values in the associative array
     $uniqueLetterCount = array_fill_keys(array_keys($uniquesIndexes), 0);
 
-    //count the occurrences for the elements in letterPairs array, stop pushing values if values start to repeat
+    //count the occurrences for the elements in letterPairs array
     $uniquePairsFinal = [];
     foreach ($letterPairs as $letterUniquePair) {
         if (array_key_exists($letterUniquePair, $uniqueLetterCount) == true) {
             $uniqueLetterCount[$letterUniquePair]++;
             array_push($uniquePairsFinal, $letterUniquePair);
+
+            //stop pushing values if values start to repeat
             if (max($uniqueLetterCount) > 1) {
                 array_pop($uniquePairsFinal);
                 break;
             }
         }
     }
-    //count how many occurrences happened without them repeating,
+    //count how many occurrences happened without repeating,
     $outputStringLength = (sizeof($uniquePairsFinal));
     $lettersForOutput = [];
 
@@ -83,6 +87,7 @@ function longestSubstr(string $string): string
     return implode($lettersForOutput);
 }
 
+echo 'longestSubstr result: ' .PHP_EOL;
 echo longestSubstr('aaa') . PHP_EOL;
 echo longestSubstr('aZa') . PHP_EOL;
 echo longestSubstr('aZAzaz') . PHP_EOL;
--- information-technology/solutions2.php
+++ information-technology/solutions2.php
@@ -1,56 +1,63 @@
 <?php
 
 
-function maxProfit(array $pricesAndPurchases): int
-{
-
-    $prices = [];
-    $purchases = [];
-
-    //separate values for price and purchases, store each of them in array
-    foreach ($pricesAndPurchases as $priceAndPurchase) {
-        array_push($prices, $priceAndPurchase['price']);
-        array_push($purchases, $priceAndPurchase['purchased']);
-    }
-
-    $balance = 0;
-    $inventoryStock = 0;
-
-    while (sizeof($prices) > 1) {
-        //find the max selling price in a array
-        $maxPrice = array_keys($prices, max($prices));
-
-        //buy all the products, including at first instance of max price, then sell all at max price
-        for ($x = 0; $x <= $maxPrice[0]; $x++) {
-            $balance -= $prices[$x] * $purchases[$x];
-            $inventoryStock += $purchases[$x];
-        }
-        //update balance and inventory after selling all the stock
-        $balance += $prices[$maxPrice[0]] * $inventoryStock;
-        $inventoryStock = 0;
-
-        //remove completed transactions
-        for ($x = 0; $x <= $maxPrice[0]; $x++) {
-            array_shift($prices);
-            array_shift($purchases);
-        }
-    }
-    return $moneyEarned = abs($balance);
-}
-
-
-$example = [
-    0 => ['price' => 2, 'purchased' => 3],
-    1 => ['price' => 3, 'purchased' => 0],
-    2 => ['price' => 1, 'purchased' => 1],
-    3 => ['price' => 5, 'purchased' => 4],
-    4 => ['price' => 3, 'purchased' => 1],
-    5 => ['price' => 2, 'purchased' => 2]
-];
-
-echo maxProfit($example) . PHP_EOL;
-
-
+//function maxProfit(array $pricesAndPurchases): int
+//{
+//
+//    $prices = [];
+//    $purchases = [];
+//
+//    //separate values for price and purchases, store each of them in array
+//    foreach ($pricesAndPurchases as $priceAndPurchase) {
+//        array_push($prices, $priceAndPurchase['price']);
+//        array_push($purchases, $priceAndPurchase['purchased']);
+//    }
+//
+//    $balance = 0;
+//    $inventoryStock = 0;
+//
+//    while (sizeof($prices) > 1) {
+//        //find the max selling price in a array
+//        $maxPrice = array_keys($prices, max($prices));
+//
+//        //buy all the products, including at first instance of max price, then sell all at max price
+//        for ($x = 0; $x <= $maxPrice[0]; $x++) {
+//            $balance -= $prices[$x] * $purchases[$x];
+//            $inventoryStock += $purchases[$x];
+//        }
+//        //update balance and inventory after selling all the stock
+//        $balance += $prices[$maxPrice[0]] * $inventoryStock;
+//        $inventoryStock = 0;
+//
+//        //remove completed transactions
+//        for ($x = 0; $x <= $maxPrice[0]; $x++) {
+//            array_shift($prices);
+//            array_shift($purchases);
+//        }
+//    }
+//    return $balance;
+//}
+//
+//echo 'maxProfit result: ' .PHP_EOL;
+//$example = [
+//    0 => ['price' => 2, 'purchased' => 1],
+//    1 => ['price' => 8, 'purchased' => 1],
+//    2 => ['price' => 10, 'purchased' => 1],
+//    3 => ['price' => 4, 'purchased' => 1],
+//    4 => ['price' => 9, 'purchased' => 1]
+//
+//];
+//$example2 = [
+//    0 => ['price' => 2, 'purchased' => 3],
+//    1 => ['price' => 3, 'purchased' => 0],
+//    2 => ['price' => 1, 'purchased' => 1],
+//    3 => ['price' => 5, 'purchased' => 4],
+//    4 => ['price' => 3, 'purchased' => 1],
+//    5 => ['price' => 2, 'purchased' => 2]
+//];
+//
+//echo maxProfit($example) . PHP_EOL;
+//echo maxProfit($example2) . PHP_EOL;
 
 
 //
@@ -62,9 +69,25 @@ echo maxProfit($example) . PHP_EOL;
 //    throw new \Exception('Not implemented.');
 //}
 //
-//function incrementalMedian(array $values): array
-//{
 //
-//
-//    throw new \Exception('Not implemented.');
-//}
+
+
+function incrementalMedian(array $values)
+{
+    $outputValues = [];
+    for ($x = 1; $x <= sizeof($values); $x++) {
+        $selectedValues = array_slice($values, 0, $x);
+        sort($selectedValues);
+        if (sizeof($selectedValues) % 2 == 0) {
+            array_push($outputValues, ((float)$selectedValues[sizeof($selectedValues) / 2 - 1]));
+        } else {
+            array_push($outputValues, (float)$selectedValues[sizeof($selectedValues) / 2]);
+        }
+    }
+
+    return $outputValues;
+
+}
+
+$medianExample1 = [1, 8, 4, 7, 13];
+var_dump(incrementalMedian($medianExample1));
